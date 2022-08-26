b45b246e98d346810e926d0df4fa4b5894a3c7df
skan0017 <skan0017@student.monash.edu>
2022-08-26 22:16:38 +1000

    quarternion transform finished

--- waypoint_generator/waypoint_generator/waypoint_publisher_node.py
+++ waypoint_generator/waypoint_generator/waypoint_publisher_node.py
@@ -1,18 +1,20 @@
+#!/usr/bin/python3 
 import rclpy
 from rclpy.node import Node
 from planning_interfaces.msg import Waypoint, WaypointArray
 from visualization_msgs.msg import Marker, MarkerArray
-from geometry_msgs.msg import Transform, PoseStamped
+from geometry_msgs.msg import Transform, Pose
 from tf2_msgs.msg import TFMessage
+import tf2_ros as tf2
 import math 
 import numpy as np
-
+from tf_transformations import quaternion_multiply
 
 class WaypointPublisher(Node):
     def __init__(self):
         super().__init__('waypoint_publisher')
-        self.waypoints_publisher_ = self.create_publisher(WaypointArray,'global_waypoint',10)
-        self.waypoints_visualiser_ = self.create_publisher(MarkerArray,'global_waypoint_marker',0)
+        self.waypoints_publisher_ = self.create_publisher(WaypointArray,'global_waypoints',10)
+        self.waypoints_visualiser_ = self.create_publisher(MarkerArray,'global_waypoint_markers',0)
         self.tf_subscriber_ = self.create_subscription(TFMessage,  "/tf_static",self.tf_received,10)
         timer_period = 0.5
         self.num_trajectories = 10 # number of trajectories desired
@@ -20,17 +22,19 @@ class WaypointPublisher(Node):
         self.points_array = np.zeros((self.num_trajectories,self.sampling_points,3))
         self.grid_size = (3,2) #(width, height -> y,x using robotics coordinate frame)
         self.timer = self.create_timer(timer_period,self.timer_callback)
+        self.tfbuffer = tf2.Buffer()
         self.waypoints = WaypointArray()
         self.waypoint_markers = MarkerArray()
         self.base_link_height = 0.0
         self.baselnk_2_camera = Transform()
+        self.published = False
         print("finished creating trajectories!")
 
     def connect_endpoints(self,point1, point2):
         """
         Connect a set of two cartesian endpoints parabolically using a fixed number of sample.
-            point1    -> first endpoint
-            point2    -> second endpoint
+            point1 -> first endpoint
+            point2 -> second endpoint
         """
         m = (point2[1] - point1[1])/(np.cosh(point2[0]) - np.cosh(point1[0]))
         c = point1[1] - m*np.cosh(point1[0])
@@ -86,21 +90,18 @@ class WaypointPublisher(Node):
                 self.waypoint_markers.markers.append(marker)
                 id_count+=1
     
-    def convert_pose(self,pose,from_frame):
+    def convert_pose(self,pose):
         """
         Convert a pose or transform between frames using tf.
-        pose            -> A geometry_msgs.msg/Pose that defines the robots position and orientation in a reference_frame
-        from_frame      -> A string that defines the original reference_frame of the robot
-        to_frame        -> A string that defines the desired reference_frame of the robot to convert to
+        pose -> A geometry_msgs.msg/Pose that defines the robots position and orientation in a reference_frame
         """
-        spose = PoseStamped()
-        spose.pose = pose
-        spose.header.stamp = Node.get_clock(self).now().to_msg()
-        spose.header.frame_id = from_frame
+        out_pose = Pose()
+        out_pose.position.x = pose.position.x + self.baselnk_2_camera.translation.x
+        out_pose.position.y = pose.position.y + self.baselnk_2_camera.translation.y
+        out_pose.position.z = pose.position.z + self.baselnk_2_camera.translation.z
+        out_pose.quarternion = quaternion_multiply(pose.orientation,self.baselnk_2_camera.rotation)
 
-        p2 = tf2_geometry_msgs.do_transform_pose(spose, self.baselnk_2_camera)
-
-        return p2.pose
+        return out_pose
     
     def tf_received(self,msg):
         """
@@ -112,25 +113,32 @@ class WaypointPublisher(Node):
             baselnk_2_camera.rotation = msg.transforms[1].transform.rotation ## combine rotation and translation to create one tf between base_link and depth_camera
             self.baselnk_2_camera = baselnk_2_camera
             self.tf_subscriber_.destroy() ## destroy subscription after storing transformations
+            
     
     def set_camera_waypoints(self):
         """
         Convert world waypoints to camera waypoints
         """
         for idx, waypoint in enumerate(self.waypoints.waypoints):
-            camera_pose = self.convert_pose(waypoint.pose,"base_link")
-            self.waypoints[idx].pose = camera_pose
-            self.waypoints[idx].frame_id = "camera_depth_link"
+            camera_pose = self.convert_pose(waypoint.pose)
+            self.waypoints.waypoints[idx].pose = camera_pose
+            self.waypoints.waypoints[idx].frame_id = "camera_depth_link"
 
     def timer_callback(self):
         """
-        Run steps for waypoint generation and publish 
+        Run steps for waypoint generation and publish
         """
-        self.generate_points()
-        self.set_world_waypoints()
-        self.set_camera_waypoints()
-        self.waypoints_publisher_.publish(self.waypoints)
-        self.waypoints_visualiser_.publish(self.waypoint_markers)
+        if not self.published:
+            self.generate_points()
+            self.set_world_waypoints()
+            self.set_camera_waypoints()
+            self.waypoints_publisher_.publish(self.waypoints)
+            self.waypoints_visualiser_.publish(self.waypoint_markers)
+            self.published = True
+        else:
+            self.waypoints_publisher_.publish(self.waypoints)
+            self.waypoints_visualiser_.publish(self.waypoint_markers)
+        
     
 def main(args=None):
     rclpy.init(args=args)
