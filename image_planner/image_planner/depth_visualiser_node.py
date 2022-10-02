d04aa95afdcde6389ab9527a44330169d592d8e4
skan0017 <skan0017@student.monash.edu>
2022-10-02 22:59:44 +1100

    collision free trajectories implemented

--- image_planner/image_planner/depth_visualiser_node.py
+++ image_planner/image_planner/depth_visualiser_node.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-from re import S
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
-from geometry_msgs.msg import PoseStamped
-from nav_msgs.msg import Odometry
 import numpy as np
 import ros2_numpy 
 import image_geometry
 from planning_interfaces.msg import WaypointArray, WaypointInfo
 from tf2_msgs.msg import TFMessage
 from image_planner.utils import transforms
+from planning_interfaces.msg import TentacleArray
         
 class DepthVisualiser(Node):
     def __init__(self):
@@ -20,77 +18,66 @@ class DepthVisualiser(Node):
         self.tf_subscriber_ = self.create_subscription(TFMessage,"/tf_static",self.tf_received,10)
         self.camera_info_subscriber_ = self.create_subscription(CameraInfo,"/camera/depth/camera_info",self.camera_info_received,10)
         self.waypoint_subscriber_ = self.create_subscription(WaypointArray,"/local_waypoints",self.waypoints_received,10)
-        self.goal_subscriber_ = self.create_subscription(PoseStamped,"/goal_pose",self.goal_received,10)
         self.waypoint_info_subscriber_ = self.create_subscription(WaypointInfo,"/local_waypoint_info",self.waypoints_info_received,10)
-        self.pose_subscriber_ = self.create_subscription(Odometry,"/odom",self.odom_received,10)
         self.image_publisher_ = self.create_publisher(Image, '/waypoint_overlay', 10)
+        self.tentacle_publisher_ = self.create_publisher(TentacleArray, '/Tentacle_selection', 10)
         # timer_period = 1/10
         # self.timer = self.create_timer(timer_period,self.timer_callback)
-        self.points_array = None
         self.camera_traj_pixels = None
         self.z_truth = None
-        self.global_waypoint_pixel = np.array([[0],[0],[0],[1]])
-        self.pose = np.array([0,0])
         self.camera_model = None 
         self.baselnk_2_camera = None
+        self.visualise = True
         self.IM_HEIGHT = 480
         self.IM_WIDTH = 640
         self.num_trajectories = 0
         self.num_samples = 0
+        self.crit_dist = 0.4
 
     # def timer_callback(self):
     #     if self.points_array is not None and :
 
-    def odom_received(self,msg):
-        self.pose = np.array([msg.pose.pose.position.x,msg.pose.pose.position.y])
-    
-    def goal_received(self,msg):
-        if msg is not None and self.baselnk_2_camera is not None and self.camera_model is not None:
-            print(self.pose)
-            global_waypoint = np.array([[msg.pose.position.x-self.pose[0]],[msg.pose.position.y-self.pose[1]],[0],[1]])
-            global_waypoint_pixel = transforms.transform_points(self.baselnk_2_camera, global_waypoint)
-            self.global_waypoint_pixel = transforms.project_points(self.camera_model.intrinsicMatrix(),global_waypoint_pixel)
-            self.global_waypoint_pixel[0] = np.clip(self.global_waypoint_pixel[0], a_min = 0, a_max = self.IM_WIDTH)
-            self.global_waypoint_pixel[1] = np.clip(self.global_waypoint_pixel[1], a_min = 0, a_max = self.IM_HEIGHT)
-
-    def image_received(self,msg):
-        current_frame = ros2_numpy.numpify(msg)
-        if self.camera_traj_pixels is not None:
-            current_frame[self.camera_traj_pixels[1,:],self.camera_traj_pixels[0,:],:] = np.array([255,0,0])
-            current_frame[self.global_waypoint_pixel[1],self.global_waypoint_pixel[0],:] = np.array([0,255,0])
-            img = ros2_numpy.msgify(Image, current_frame,encoding="rgb8")
-            self.image_publisher_.publish(img)
-
+    def tentacle_select(self,measured_depth):
+        if self.camera_traj_pixels is not None and self.z_truth is not None:
+            z_measure = measured_depth[self.camera_traj_pixels[1,:],self.camera_traj_pixels[0,:]]
+            diff = 0.5*self.z_truth - z_measure
+            diff = np.reshape(diff,(self.num_trajectories,self.num_samples))
+            diff[diff > self.crit_dist] = 1
+            diff[diff <= self.crit_dist] = 0
+            diff_traj = diff.sum(axis=1)
+            diff_traj = diff_traj[::-1]
+            
+            
     def depth_received(self,msg):
+        '''
+        Following https://www.ros.org/reps/rep-0118.html
+        '''
         depth_frame = ros2_numpy.numpify(msg)
         depth_frame = np.array(depth_frame, dtype=np.float32)
         self.tentacle_select(depth_frame)
-        # center_idx = np.array(depth_frame.shape) // 2
-        # print ('center depth:', depth_frame[center_idx[0], center_idx[1]])
 
-    def tentacle_select(self,measured_depth):
-        if self.camera_traj_pixels is not None:
-            z_measure = measured_depth[self.camera_traj_pixels[1,:],self.camera_traj_pixels[0,:]]
-            print([z_measure.shape])
-            # diff = z_measure-self.z_truth
-            # print(diff)
+    def image_received(self,msg):
+        if self.camera_traj_pixels is not None and self.visualise is True:
+            current_frame = ros2_numpy.numpify(msg)
+            current_frame[self.camera_traj_pixels[1,:],self.camera_traj_pixels[0,:],:] = np.array([255,0,0])
+            img = ros2_numpy.msgify(Image, current_frame,encoding="rgb8")
+            self.image_publisher_.publish(img)
 
-    
     def waypoints_received(self,msg):
         if msg is not None and self.num_samples > 0 and self.baselnk_2_camera is not None and self.camera_model is not None:
-            self.points_array = np.zeros(shape=(self.num_samples*self.num_trajectories,3))
+            points_array = np.zeros(shape=(self.num_samples*self.num_trajectories,3))
             waypoints = msg.waypoints
 
             for index, waypoint in enumerate(waypoints):
                 mat = ros2_numpy.numpify(waypoint.pose)
-                self.points_array[index,:] = mat[:-1,-1]
+                points_array[index,:] = mat[:-1,-1]
             
-            self.points_array= self.points_array.T
-            homogen = np.ones(self.points_array.shape[1])
-            self.points_array = np.vstack([self.points_array,homogen])
-            camera_points = transforms.transform_points(self.baselnk_2_camera, self.points_array)
-            self.camera_traj_pixels = transforms.project_points(self.camera_model.intrinsicMatrix(),camera_points)
-            self.z_truth = self.points_array[0,:]
+            points_array= points_array.T
+            homogen = np.ones(points_array.shape[1])
+            points_array = np.vstack([points_array,homogen])
+            camera_points_array = transforms.transform_points(self.baselnk_2_camera, points_array)
+            self.camera_traj_pixels = transforms.project_points(self.camera_model.intrinsicMatrix(),camera_points_array)
+            self.z_truth = camera_points_array[-2,:]
             self.waypoint_subscriber_.destroy() ## destroy subscription after storing waypoints
     
     def waypoints_info_received(self,msg):
